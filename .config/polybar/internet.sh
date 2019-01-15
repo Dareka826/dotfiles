3df17501036a9ebe2f324db52d9f371fed4f4754
Dareka826 <minecraftredstonemaniak@gmail.com>
2019-01-15 19:05:56 +0100

    Create internet.sh

---
+++ .config/polybar/internet.sh
@@ -0,0 +1,18 @@
+foreground="#FFFFFF"
+netCol="#222222" #Network color
+
+interfaces="$(ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p' | paste -sd " ")"
+int1="$(echo $interfaces | cut -d " " -f2)"
+int2="$(echo $interfaces | cut -d " " -f3)"
+
+if iwconfig $int1 >/dev/null 2>&1
+then
+    wifi=$int1
+    eth0=$int2
+else
+    wifi=$int2
+    eth0=$int1
+fi
+ip link show $eth0 | grep 'state UP' >/dev/null && int=$eth0 || int=$wifi
+
+ping -c 1 8.8.8.8 >/dev/null 2>&1 && printf "%s" "%{F${netCol}}%{F${foreground} B${netCol}} $int connected " || echo "%{F${netCol}}%{F${foreground} B${netCol}} $int disconnected "
