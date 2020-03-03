4d21186ac708230a19bbc3b82e6e4410b1d715eb
Dareka826 <minecraftredstonemaniak@gmail.com>
2020-03-03 18:41:27 +0100

    Delete clock.sh

--- .config/polybar/clock.sh
+++
@@ -1,5 +0,0 @@
-foreground=#FFFFFF
-dateCol="#222222" #Date color
-timeCol="#5000AA" #Time color
-
-printf "%s" "%{F${dateCol}}%{F${foreground} B${dateCol}} $(date +%d/%m/%Y) %{B${dateCol} F${timeCol}}%{B${timeCol} F${foreground}} $(date +%H:%M:%S) "
