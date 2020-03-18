9e976b33aedeceaa6a75d2750e4f0882836e10b9
Rin <minecraftredstonemaniak@gmail.com>
2020-03-18 11:56:40 +0100

    Update internet.sh

--- .config/polybar/internet.sh
+++ .config/polybar/internet.sh
@@ -1,19 +1,14 @@
 foreground="#FFFFFF"
 foreOff="#FF0000"
-netCol="#222222" #Network color
+background="#222222"
 
-interfaces="$(ip link | sed -n 's/^[0-9]: \(.*\):.*$/\1/p' | paste -sd " ")"
-int1="$(echo $interfaces | cut -d " " -f2)"
-int2="$(echo $interfaces | cut -d " " -f3)"
+INT="enp5s0"
+ADDR="8.8.8.8"
 
-if iwconfig $int1 >/dev/null 2>&1
+printf "%%{B$background}"
+if [[ "$(ping -c 1 $ADDR 2>&1 | grep '100% packet loss')" != "" ]]
 then
-    wifi=$int1
-    eth0=$int2
+	printf "%%{F$foreOff} $INT disconnected %%{F$foreground}"
 else
-    wifi=$int2
-    eth0=$int1
+	printf "%%{F$foreground} $INT connected "
 fi
-ip link show $eth0 | grep 'state UP' >/dev/null && int=$eth0 || int=$wifi
-
-ping -c 1 archlinux.org >/dev/null 2>&1 && printf "%s" "%{F${netCol}}%{F${foreground} B${netCol}} $int connected " || echo "%{F${netCol}}%{F${foreOff} B${netCol}} $int disconnected "
