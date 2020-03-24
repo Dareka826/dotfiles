565a4340ae0432d0efacd5f8db3162a9e512888e
Rin <minecraftredstonemaniak@gmail.com>
2020-03-24 10:45:17 +0100

    Fix indentation

--- .config/polybar/cmus.sh
+++ .config/polybar/cmus.sh
@@ -7,13 +7,13 @@ $1=="tag" && $2=="tracknumber" { tracknum = $3 }
 $1=="duration" { dur = $2 }
 $1=="position" { pos = $2 }
 function stohms(t) {
-    s = t%60
-    m = ((t-s)/60)%60
-    h = (t-m*60-s)/3600
-    if(h<10) h = "0"h
-    if(m<10) m = "0"m
-    if(s<10) s = "0"s
-    return h ":" m ":" s
+	s = t%60
+	m = ((t-s)/60)%60
+	h = (t-m*60-s)/3600
+	if(h<10) h = "0"h
+	if(m<10) m = "0"m
+	if(s<10) s = "0"s
+	return h ":" m ":" s
 }
 END {
 	if(dur!=0)
