0487e735d593e825625ee6b8961cc8f77b297d68
Rin <minecraftredstonemaniak@gmail.com>
2020-03-26 21:06:33 +0100

    Update internet.sh

--- .config/polybar/internet.sh
+++ .config/polybar/internet.sh
@@ -1,14 +1,7 @@
 foreground="#FFFFFF"
 foreOff="#FF0000"
 background="#222222"
-
-INT="enp5s0"
 ADDR="8.8.8.8"
 
 printf "%%{B$background}"
-if [[ "$(ping -c 1 $ADDR 2>&1 | grep '100% packet loss')" != "" ]]
-then
-	printf "%%{F$foreOff} $INT disconnected %%{F$foreground}"
-else
-	printf "%%{F$foreground} $INT connected "
-fi
+[ "$(ping -c 1 $ADDR 2>&1 | grep '100% packet loss')" != "" ] && printf "%%{F$foreOff} Network disconnected %%{F$foreground}" || printf "%%{F$foreground} Network connected "
