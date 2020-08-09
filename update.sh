6227032f5c5695f45396db0379a9a420faa425ca
Dareka826 <minecraftredstonemaniak@gmail.com>
2020-08-09 22:26:38 +0200

    New update file

--- update.sh
+++ update.sh
@@ -1,70 +1,78 @@
 #!/bin/sh
 
 rm -rf df
+
 mkdir df
+cp ~/.zshrc									df/
+cp ~/.vimrc									df/
+cp ~/.tmux.conf								df/
+cp ~/.xinitrc								df/
+cp ~/.Xresources							df/
+cp ~/.palette.txt							df/
 
-# ZSH
-cp ~/.zshrc				df/.zshrc
-# VIM
-cp ~/.vimrc				df/.vimrc
-cp ~/.ycm_extra_conf.py	df/.ycm_extra_conf.py
-# XINITRC
-cp ~/.xinitrc			df/.xinitrc
-# XRESOURCES
-cp ~/.Xresources			df/.Xresources
+mkdir df/.vim
+cp ~/.vim/.ycm_extra_conf.py				df/.vim/
 
-# ~/.config
-mkdir -p df/.config/alacritty
-mkdir -p df/.config/cmus
-mkdir -p df/.config/dunst
-mkdir -p df/.config/i3
-mkdir -p df/.config/mpv
+mkdir df/.config
+cp ~/.config/chromium-flags.conf			df/.config/
+cp ~/.config/picom.conf						df/.config/
+
+mkdir df/.config/alacritty
+cp ~/.config/alacritty/alacritty.yml		df/.config/alacritty/
+
+mkdir df/.config/cava
+cp ~/.config/cava/config					df/.config/cava/
+
+mkdir df/.config/cmus
+cp ~/.config/cmus/drk826.theme				df/.config/cmus/
+
+mkdir df/.config/dunst
+cp ~/.config/dunst/dunstrc					df/.config/dunst/
+
+mkdir df/.config/gtk-3.0
+cp ~/.config/gtk-3.0/gtk.css				df/.config/gtk-3.0/
+
+mkdir df/.config/i3
+cp ~/.config/i3/config						df/.config/i3/
+
+mkdir df/.config/mpv
+cp ~/.config/mpv/mpv.conf					df/.config/mpv/
+cp ~/.config/mpv/input.conf					df/.config/mpv/
+
 mkdir -p df/.config/oomox/colors
-mkdir -p df/.config/polybar
-mkdir -p df/.config/ranger
-mkdir -p df/.config/vis/colors
-mkdir -p df/.config/xfce4/terminal
-mkdir -p df/.config/gtk-3.0
-mkdir -p df/.config/kitty
+cp ~/.config/oomox/colors/*					df/.config/oomox/colors/
 
-# alacritty
-cp ~/.config/alacritty/alacritty.yml						df/.config/alacritty/alacritty.yml
-# CMUS
-cp ~/.config/cmus/drk826.theme								df/.config/cmus/drk826.theme
-# dunst
-cp ~/.config/dunst/dunstrc									df/.config/dunst/dunstrc
-# i3
-cp ~/.config/i3/config										df/.config/i3/config
-# mpv
-cp ~/.config/mpv/mpv.conf									df/.config/mpv/mpv.conf
-cp ~/.config/mpv/input.conf									df/.config/mpv/input.conf
-# oomox
-cp ~/.config/oomox/colors/Rin								df/.config/oomox/colors/Rin
-cp ~/.config/oomox/colors/RinN								df/.config/oomox/colors/RinN
-# polybar
-cp ~/.config/polybar/*										df/.config/polybar/
-# ranger
-cp ~/.config/ranger/rc.conf									df/.config/ranger/rc.conf
-# vis
-cp ~/.config/vis/colors/*									df/.config/vis/colors/
-cp ~/.config/vis/config										df/.config/vis/config
-# xfce4-terminal
-cp ~/.config/xfce4/terminal/terminalrc						df/.config/xfce4/terminal/terminalrc
-# chromium flags
-cp ~/.config/chromium-flags.conf							df/.config/chromium-flags.conf
-# picom
-cp ~/.config/picom.conf										df/.config/picom.conf
+mkdir df/.config/polybar
+cp ~/.config/polybar/*						df/.config/polybar/
 
-# gtk.css
-cp ~/.config/gtk-3.0/gtk.css								df/.config/gtk-3.0/gtk.css
+mkdir df/.config/ranger
+cp ~/.config/ranger/rc.conf					df/.config/ranger/
 
-# kitty
-cp ~/.config/kitty/kitty.conf								df/.config/kitty/kitty.conf
+mkdir df/.minecraft
+cp ~/.minecraft/options.txt					df/.minecraft/
+cp ~/.minecraft/optionsof.txt				df/.minecraft/
+cp ~/.minecraft/optionsshaders.txt			df/.minecraft/
+cp ~/.minecraft/.seeds.txt					df/.minecraft/
 
-# firefox
-cp ~/.mozilla/firefox/a4a2pjuk.default-release/chrome/*		df/.mozilla/firefox/ur-profile/chrome/
+mkdir df/Wallpapers
+cp ~/Wallpapers/lockscreen1.png				df/Wallpapers/
+cp ~/Wallpapers/lockscreen1.svg				df/Wallpapers/
 
+mkdir df/themes
+cp ~/themes/*								df/themes/
 
-# Lockscreen
-cp ~/Wallpapers/lockscreen*									df/Wallpapers/
-cp ~/.local/bin/lock.sh										df/.local/bin/lock.sh
+mkdir -p df/.local/bin
+cp ~/.local/bin/_save-i3.sh					df/.local/bin/
+cp ~/.local/bin/_restore-i3.sh				df/.local/bin/
+cp ~/.local/bin/animated-wallpaper			df/.local/bin/
+cp ~/.local/bin/animated-wallpaper-pan		df/.local/bin/
+cp ~/.local/bin/animated-wallpaper-stop		df/.local/bin/
+cp ~/.local/bin/baudline_wrapper			df/.local/bin/
+cp ~/.local/bin/convert-smol				df/.local/bin/
+cp ~/.local/bin/lock.sh						df/.local/bin/
+cp ~/.local/bin/lock_sus.sh					df/.local/bin/
+cp ~/.local/bin/mahjongsoul					df/.local/bin/
+cp ~/.local/bin/resetusb					df/.local/bin/
+cp ~/.local/bin/screenshot					df/.local/bin/
+cp ~/.local/bin/controller-battery.sh		df/.local/bin/
+
