d93781c718d08723d0579dd0a3fd3fc8ce22376d
Dareka826 <minecraftredstonemaniak@gmail.com>
2020-08-09 22:52:10 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,40 +1,56 @@
 #!/bin/sh
 
-# zsh
-cp -i df/.zshrc ~/.zshrc
+# Zsh
+cp -i df/.zshrc							~/
 curl -L git.io/antigen > ~/.antigen.zsh
 
-# vim
-cp -i df/.vimrc ~/.vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Vim
+cp -i df/.vimrc							~/
 mkdir -p ~/.vim/colors
-curl -L https://raw.githubusercontent.com/ciaranm/inkpot/master/colors/inkpot.vim -o ~/.vim/colors/inkpot.vim
 curl -L https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o ~/.vim/colors/molokai.vim
-cp -i df/.ycm_extra_conf.py ~/.ycm_extra_conf.py
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp -i df/.vim/.ycm_extra_conf.py		~/.vim/
 
-# xinitrc
-cp -i df/.xinitrc ~/.xinitrc
-# xresources
-cp -i df/.Xresources ~/.Xresources
+# X files
+cp -i df/.xinitrc						~/
+cp -i df/.Xresources					~/
 
-# ~/.config
-cp -ri df/.config ~/
+# Tmux
+cp -i df/.tmux.conf						~/
 
-# wal + wallpaper
-mkdir -p ~/Wallpapers
+# Color palette
+cp -i df/.palette.txt					~/
+
+# Config
+cp -ri df/.config						~/
+
+# Wallpapers
+mkdir ~/Wallpapers
+cp df/Wallpapers/*						~/Wallpapers
 curl -L https://i.imgur.com/z4ffOWb.png -o ~/Wallpapers/71123408_p0.png
+
+# Wal
 wal -i ~/Wallpapers/71123408_p0.png
 
-# oomox
-# oomox-cli -m all ~/.config/oomox/colors/Rin
+# Oomox
+# oomox-cli -m all ~/.config/oomox/colors/RinN
 
-# cursor
-mkdir -p ~/.icons
+# Suwako cursor
+mkdir ~/.icons
 cd ~/.icons
 curl -LO https://github.com/Dareka826/Suwako_Cursor_Linux_Port/raw/master/Suwako_Cursor.tar.gz
 tar -xf Suwako_Cursor.tar.gz
 rm -rf Suwako_Cursor.tar.gz
 
-# custom scripts
+# Custom scripts
 mkdir -p ~/.local/bin
-cp -i df/.local/bin/* ~/.local/bin/
+cp -i df/.local/bin/*					~/.local/bin/
+
+# Themes
+mkdir ~/themes
+cp -i df/themes/*						~/themes/
+
+# Minecraft
+mkdir ~/.minecraft
+cp -i df/.minecraft/*					~/.minecraft/
+
