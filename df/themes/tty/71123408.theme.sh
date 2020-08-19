2bc530daf476392e9c1dbd77398ab952fa14a3b2
Dareka826 <minecraftredstonemaniak@gmail.com>
2020-08-19 19:02:05 +0200

    Remove theme tty checks

--- df/themes/tty/71123408.theme.sh
+++ df/themes/tty/71123408.theme.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-[ "$TERM" = "linux" ] && \
-	printf '%b' '\e]P00C0D1E
-	             \e]P8A69DA3
-	             \e]P1404385
-	             \e]P9404385
-	             \e]P2625F99
-	             \e]PA625F99
-	             \e]P38F729D
-	             \e]PB8F729D
-	             \e]P4B77AA3
-	             \e]PCB77AA3
-	             \e]P5A491AC
-	             \e]PDA491AC
-	             \e]P6D0AFC3
-	             \e]PED0AFC3
-	             \e]P7EEE1E9
-	             \e]PFEEE1E9
-				 \ec'
+printf '%b' '\e]P00C0D1E
+			 \e]P8A69DA3
+			 \e]P1404385
+			 \e]P9404385
+			 \e]P2625F99
+			 \e]PA625F99
+			 \e]P38F729D
+			 \e]PB8F729D
+			 \e]P4B77AA3
+			 \e]PCB77AA3
+			 \e]P5A491AC
+			 \e]PDA491AC
+			 \e]P6D0AFC3
+			 \e]PED0AFC3
+			 \e]P7EEE1E9
+			 \e]PFEEE1E9
+			 \ec'
--- df/themes/tty/Atelierlakeside.theme.sh
+++ df/themes/tty/Atelierlakeside.theme.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-[ "$TERM" = "linux" ] && \
-	printf '%b' '\e]P0161B1D
-				 \e]P85A7B8C
-				 \e]P1D22D72
-				 \e]P9D22D72
-				 \e]P2568C3B
-				 \e]PA568C3B
-				 \e]P38A8A0F
-				 \e]PB8A8A0F
-				 \e]P4257FAD
-				 \e]PC257FAD
-				 \e]P55D5DB1
-				 \e]PD5D5DB1
-				 \e]P62D8F6F
-				 \e]PE2D8F6F
-				 \e]P77EA2B4
-				 \e]PFEBF8FF
-				 \ec'
+printf '%b' '\e]P0161B1D
+			 \e]P85A7B8C
+			 \e]P1D22D72
+			 \e]P9D22D72
+			 \e]P2568C3B
+			 \e]PA568C3B
+			 \e]P38A8A0F
+			 \e]PB8A8A0F
+			 \e]P4257FAD
+			 \e]PC257FAD
+			 \e]P55D5DB1
+			 \e]PD5D5DB1
+			 \e]P62D8F6F
+			 \e]PE2D8F6F
+			 \e]P77EA2B4
+			 \e]PFEBF8FF
+			 \ec'
