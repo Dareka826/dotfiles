a0a4c2f7617dadfa4e37e14301f9be142939cb9f
Dareka826 <minecraftredstonemaniak@gmail.com>
2020-09-04 21:37:39 +0200

    Added terminal theme

---
+++ df/themes/Aoi_comfy.theme.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo -ne   '\e]11;#201610\e\\' #BG
+echo -ne   '\e]10;#F1D2B9\e\\' #FG
+echo -ne  '\e]4;0;#201610\e\\' #C0
+echo -ne  '\e]4;8;#A89381\e\\' #C8
+echo -ne  '\e]4;1;#A17152\e\\' #C1
+echo -ne  '\e]4;9;#A17152\e\\' #C9
+echo -ne  '\e]4;2;#927965\e\\' #C2
+echo -ne '\e]4;10;#927965\e\\' #C10
+echo -ne  '\e]4;3;#AC8767\e\\' #C3
+echo -ne '\e]4;11;#AC8767\e\\' #C11
+echo -ne  '\e]4;4;#CE976D\e\\' #C4
+echo -ne '\e]4;12;#CE976D\e\\' #C12
+echo -ne  '\e]4;5;#DD9E67\e\\' #C5
+echo -ne '\e]4;13;#DD9E67\e\\' #C13
+echo -ne  '\e]4;6;#FBD177\e\\' #C6
+echo -ne '\e]4;14;#FBD177\e\\' #C14
+echo -ne  '\e]4;7;#F1D2B9\e\\' #C7
+echo -ne '\e]4;15;#F1D2B9\e\\' #C15
---
+++ df/themes/tty/Aoi_comfy.theme.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+[ "$TERM" = "linux" ] && \
+	printf '%b' '\e]P0201610
+				 \e]P8A89381
+				 \e]P1A17152
+				 \e]P9A17152
+				 \e]P2927965
+				 \e]PA927965
+				 \e]P3AC8767
+				 \e]PBAC8767
+				 \e]P4CE976D
+				 \e]PCCE976D
+				 \e]P5DD9E67
+				 \e]PDDD9E67
+				 \e]P6FBD177
+				 \e]PEFBD177
+				 \e]P7F1D2B9
+				 \e]PFF1D2B9
+				 \ec'
