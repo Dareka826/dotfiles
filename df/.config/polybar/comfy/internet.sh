8e8e6d74e0399624043096473b212ea7aefaaf7d
Rin Takanashi <dareka826@gmail.com>
2020-11-26 09:46:25 +0100

    Updated polybar config

--- df/.config/polybar/comfy/internet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-foreground="#201610"
-foreOff="#FF0000"
-background="#DD9E67"
-ADDR="8.8.8.8"
-
-printf "%%{B$background}"
-[ "$(ping -c 1 $ADDR 2>&1 | grep -E '(100% packet loss)|(Network is unreachable)')" != "" ] && printf "%%{F$foreOff}Ex %%{F$foreground}" || printf "%%{F$foreground}E^ "
-
--- df/.config/polybar/comfy/start-polybar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sleep 3
-polybar -c ~/.config/polybar/comfy/config.ini main
