862a3dcccf5b3199cd61df5ac21b152a1a74d126
Rin Takanashi <dareka826@gmail.com>
2021-06-04 11:08:09 +0200

    Add option for non-interactive install.sh and comment out some stuf not usually wanted

--- install.sh
+++ install.sh
@@ -1,39 +1,42 @@
 #!/bin/sh
 
+SAFETY_OPTS="-i"
+[ "$1" == "--force" ] && SAFETY_OPTS=""
+
 # Zsh
-cp -i df/.zshenv ~/
+cp $SAFETY_OPTS df/.zshenv ~/
 
 mkdir -p ~/.config/zsh
-cp -i df/.config/zsh/.zshrc ~/.config/zsh/
-cp -i df/.zshrc ~/
+cp $SAFETY_OPTS df/.config/zsh/.zshrc ~/.config/zsh/
+cp $SAFETY_OPTS df/.zshrc ~/
 
 mkdir ~/.zinit
 git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
 
 # Tmux
-cp -i df/.tmux.conf ~/
+cp $SAFETY_OPTS df/.tmux.conf ~/
 
 # Neovim
 mkdir -p ~/.config/nvim
-cp -i df/.config/nvim/init.vim ~/.config/nvim/
+cp $SAFETY_OPTS df/.config/nvim/init.vim ~/.config/nvim/
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Vifm
 mkdir -p ~/.config/vifm/colors ~/.config/vifm/scripts
-cp -i df/.config/vifm/vifmrc ~/.config/vifm/
-cp -i df/.config/vifm/colors/rin.vifm ~/.config/vifm/colors/
-cp -i df/.config/vifm/scripts/* ~/.config/vifm/scripts/
+cp $SAFETY_OPTS df/.config/vifm/vifmrc ~/.config/vifm/
+cp $SAFETY_OPTS df/.config/vifm/colors/rin.vifm ~/.config/vifm/colors/
+cp $SAFETY_OPTS df/.config/vifm/scripts/* ~/.config/vifm/scripts/
 
 # Ranger
 #mkdir -p ~/.config/ranger
-#cp -i df/.config/ranger/rc.conf ~/.config/ranger/
+#cp $SAFETY_OPTS df/.config/ranger/rc.conf ~/.config/ranger/
 
 # Bash
-cp -i df/.bashrc ~/
+cp $SAFETY_OPTS df/.bashrc ~/
 
 # X files
-cp -i df/.xinitrc ~/
-cp -i df/.Xresources ~/
+#cp $SAFETY_OPTS df/.xinitrc ~/
+cp $SAFETY_OPTS df/.Xresources ~/
 
 # Nice Wallpapers:
 #   https://www.pixiv.net/en/artworks/71123408
@@ -45,12 +48,12 @@ cp -i df/.Xresources ~/
 
 # Console themes
 mkdir ~/themes
-cp -ir df/themes/* ~/themes/
+cp $SAFETY_OPTS -r df/themes/* ~/themes/
 
 # Suwako cursor
-mkdir ~/.icons
-cd ~/.icons
-curl -LO https://github.com/Dareka826/Suwako_Cursor_Linux_Port/raw/master/Suwako_Cursor.tar.gz
-tar -xf Suwako_Cursor.tar.gz
-rm -rf Suwako_Cursor.tar.gz
+#mkdir ~/.icons
+#cd ~/.icons
+#curl -LO https://github.com/Dareka826/Suwako_Cursor_Linux_Port/raw/master/Suwako_Cursor.tar.gz
+#tar -xf Suwako_Cursor.tar.gz
+#rm -rf Suwako_Cursor.tar.gz
 
