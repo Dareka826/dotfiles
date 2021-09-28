131672ec55a59999dc59d49ea691831c5a0692c7
Rin Takanashi <dareka826@gmail.com>
2021-09-28 23:03:14 +0200

    color-themes: add a new color theme by Chris Kempson

---
+++ color-themes/themes/Ashes.theme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -ne   '\e]11;#1C2023\e\\' #BG
+echo -ne   '\e]10;#C7CCD1\e\\' #FG
+echo -ne  '\e]4;0;#1C2023\e\\' #C0
+echo -ne  '\e]4;8;#747C84\e\\' #C8
+echo -ne  '\e]4;1;#C7AE95\e\\' #C1
+echo -ne  '\e]4;9;#C7AE95\e\\' #C9
+echo -ne  '\e]4;2;#95C7AE\e\\' #C2
+echo -ne '\e]4;10;#95C7AE\e\\' #C10
+echo -ne  '\e]4;3;#AEC795\e\\' #C3
+echo -ne '\e]4;11;#AEC795\e\\' #C11
+echo -ne  '\e]4;4;#AE97C7\e\\' #C4
+echo -ne '\e]4;12;#AE97C7\e\\' #C12
+echo -ne  '\e]4;5;#C795AE\e\\' #C5
+echo -ne '\e]4;13;#C795AE\e\\' #C13
+echo -ne  '\e]4;6;#95AEC7\e\\' #C6
+echo -ne '\e]4;14;#95AEC7\e\\' #C14
+echo -ne  '\e]4;7;#C7CCD1\e\\' #C7
+echo -ne '\e]4;15;#F3F4F5\e\\' #C15
---
+++ color-themes/themes/tty/Ashes.theme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printf '%b' '\e]P01C2023
+			 \e]P8747C84
+			 \e]P1C7AE95
+			 \e]P9C7AE95
+			 \e]P295C7AE
+			 \e]PA95C7AE
+			 \e]P3AEC795
+			 \e]PBAEC795
+			 \e]P4AE97C7
+			 \e]PCAE97C7
+			 \e]P5C795AE
+			 \e]PDC795AE
+			 \e]P695AEC7
+			 \e]PE95AEC7
+			 \e]P7C7CCD1
+			 \e]PFF3F4F5
+			 \ec'
