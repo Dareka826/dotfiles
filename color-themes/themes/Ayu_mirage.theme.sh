5f77519220c9058eee58d4f0d13b9581c28b34dc
Rin Takanashi <dareka826@gmail.com>
2021-10-09 22:39:23 +0200

    color-themes: add "ayu mirage" color theme

---
+++ color-themes/themes/Ayu_mirage.theme.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo -ne   '\e]11;#1F2430\e\\' #BG
+echo -ne   '\e]10;#FFFFFF\e\\' #FG
+echo -ne  '\e]4;0;#1F2430\e\\' #C0
+echo -ne  '\e]4;8;#444444\e\\' #C8
+echo -ne  '\e]4;1;#F28779\e\\' #C1
+echo -ne  '\e]4;9;#F28779\e\\' #C9
+echo -ne  '\e]4;2;#BAE67E\e\\' #C2
+echo -ne '\e]4;10;#BAE67E\e\\' #C10
+echo -ne  '\e]4;3;#FFCC66\e\\' #C3
+echo -ne '\e]4;11;#FFCC66\e\\' #C11
+echo -ne  '\e]4;4;#5CCFE6\e\\' #C4
+echo -ne '\e]4;12;#5CCFE6\e\\' #C12
+echo -ne  '\e]4;5;#D4BFFF\e\\' #C5
+echo -ne '\e]4;13;#D4BFFF\e\\' #C13
+echo -ne  '\e]4;6;#95E6CB\e\\' #C6
+echo -ne '\e]4;14;#95E6CB\e\\' #C14
+echo -ne  '\e]4;7;#888888\e\\' #C7
+echo -ne '\e]4;15;#FFFFFF\e\\' #C15
---
+++ color-themes/themes/tty/Ayu_mirage.theme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+printf '%b' '\e]P01F2430
+			 \e]P8444444
+			 \e]P1F28779
+			 \e]P9F28779
+			 \e]P2BAE67E
+			 \e]PABAE67E
+			 \e]P3FFCC66
+			 \e]PBFFCC66
+			 \e]P45CCFE6
+			 \e]PC5CCFE6
+			 \e]P5D4BFFF
+			 \e]PDD4BFFF
+			 \e]P695E6CB
+			 \e]PE95E6CB
+			 \e]P7888888
+			 \e]PFFFFFFF
+			 \ec'
