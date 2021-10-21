6e11169ec3277900f9456ada0da46632b4433748
Rin Takanashi <dareka826@gmail.com>
2021-10-21 19:34:07 +0200

    dwm_status.sh: add cmus display

--- scripts/.local/bin/dwm_status.sh
+++ scripts/.local/bin/dwm_status.sh
@@ -4,6 +4,32 @@ LAST_CPU_TOTAL=0
 LAST_CPU_BUSY=0
 
 while true; do
+    ##### CMUS
+    CMUS="$(cmus-remote -C status | awk '
+    $1=="file" { $1=""; sub(/.*\//,""); title = $0 }
+    $1=="tag" && $2=="title" { sub("tag title ",""); title = $0 }
+    $1=="tag" && $2=="tracknumber" { tracknum = $3 }
+    $1=="duration" { dur = $2 }
+    $1=="position" { pos = $2 }
+    function stohms(t) {
+        s = t%60
+        m = ((t-s)/60)%60
+        h = (t-m*60-s)/3600
+        if(h<10) h = "0"h
+        if(m<10) m = "0"m
+        if(s<10) s = "0"s
+        return h ":" m ":" s
+    }
+    END {
+        if(dur!=0)
+        {
+            if(tracknum == 0) tracknum = "?"
+            lef = dur-pos
+            printf("%s. %s | -%s/%s", tracknum, title, stohms(lef), stohms(dur))
+        }
+        else
+            printf("cmus off ")
+    }')"
 
     ##### CPU Usage
     # /proc/stat
@@ -40,7 +66,7 @@ while true; do
     [ $VOL_MUTED = "true" ] && VOL="M${VOL}M"
 
     ##### Display
-    xsetroot -name " C:${CPU_USAGE}% R:${MEM_USAGE}%+${SWAP_USAGE}% | V:${VOL} | $(date "+%Y-%m-%d %H:%M:%S ")"
+    xsetroot -name " [ ${CMUS} ] | C:${CPU_USAGE}% R:${MEM_USAGE}%+${SWAP_USAGE}% | V:${VOL} | $(date "+%Y-%m-%d %H:%M:%S ")"
 
     sleep 1
 done
