ed2c632cf8caa6d0818805f6b933d87e3113800a
Rin Takanashi <dareka826@gmail.com>
2022-04-30 10:03:15 +0200

    awesome: always resize floating windows by the bottom-right corner

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -609,7 +609,7 @@ clientbuttons = gears.table.join(
     end),
     awful.button({ modkey }, 3, function(c)
         c:emit_signal("request::activate", "mouse_click", {raise = true})
-        awful.mouse.client.resize(c)
+        awful.mouse.client.resize(c, "bottom_right")
     end)
 )
 
