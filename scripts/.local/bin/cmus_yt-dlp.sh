6f864800dd6a70ef78aed924bbf55acec8f4c5a2
Rin Takanashi <dareka826@gmail.com>
2022-05-30 02:58:23 +0200

    scripts: Update cmus_yt-dlp.sh

--- scripts/.local/bin/cmus_yt-dlp.sh
+++ scripts/.local/bin/cmus_yt-dlp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OUTDIR="/tmp/cmus-$(id -u)-tracks"
+OUTDIR="$HOME/.config/cmus/yt-tracks"
 install -d -o "${USER}" -g "${USER}" -m 0755 "${OUTDIR}"
 
 for ID in "$@"; do
@@ -30,6 +30,9 @@ for ID in "$@"; do
     # Calculate gain if gainless
     ffprobe "${ID_FNAME}" 2>&1 | grep 'R128_TRACK_GAIN' >/dev/null \
         || loudgain -k -s e "${ID_FNAME}"
+
+    opusinfo "${ID_FNAME}" 2>&1 | grep '^	Playback gain: 0 dB' \
+        && ~/repos/zoog/target/release/zoog --prioritize track "${ID_FNAME}"
 
     # Add the track to cmus
     cmus-remote -C "add ${ID_FNAME}"
