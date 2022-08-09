dc73a4b252ca088037f2ad5b7d92477cf63c5804
Rin Takanashi <dareka826@gmail.com>
2022-08-09 22:47:19 +0200

    scripts: Add starfield.sh (simpler version)

--- scripts/.local/bin/starfield.sh
+++ scripts/.local/bin/starfield.sh
@@ -6,18 +6,8 @@
 THRESHOLD=10
 [ -n "$1" ] && [ "$1" -gt 0 ] && [ "$1" -le 255 ] && THRESHOLD="$1"
 
-FIFO="$(mktemp -u)"
-mkfifo -m 600 "${FIFO}"
-
-# Generate 8-bit numbers
-cat /dev/urandom | od -An -tu1 | tr -s ' ' | tr ' ' '\n' | sed '/^$/d' >"${FIFO}" &
-
-# Consume the numbers
-while read VAR; do
-    [ "${VAR}" -le "${THRESHOLD}" ] \
-        && printf "." \
-        || printf " "
-
-done <"${FIFO}" | fold -w "$(tput cols)" | head -"$(( $(tput lines) - 2 ))"
-
-rm "${FIFO}"
+# Generate
+cat /dev/urandom | od -An -tu1 \
+    | tr -s ' ' | tr ' ' '\n' | sed '/^$/d' \
+    | awk "{ if (\$1 <= ${THRESHOLD}) printf(\".\"); else printf(\" \"); }" \
+    | fold -w "$(tput cols)" | head -"$(( $(tput lines) - 2 ))"
