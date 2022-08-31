1eee7f88949bb6c6a56c6d94bbc563794da31a2d
Rin Takanashi <dareka826@gmail.com>
2022-08-31 16:01:39 +0200

    colors: Add Rin-ashes

---
+++ color-themes/themes/tty/Rin-ashes.theme.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ $TERM = "linux" ] || exit 0
+
+printf '\033]P01c2023
+        \033]P8787878
+        \033]P1c8969b
+        \033]P9c8969b
+        \033]P2afc8a5
+        \033]PAafc8a5
+        \033]P3c8af96
+        \033]PBc8af96
+        \033]P4a0b9c8
+        \033]PCa0b9c8
+        \033]P5af96c8
+        \033]PDaf96c8
+        \033]P696c8b9
+        \033]PE96c8b9
+        \033]P7c8c8c8
+        \033]PFf5f5f5
+        \033c'
