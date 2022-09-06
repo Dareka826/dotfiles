9238aaace2bfaa6ad9dc08abd50c5c405b600332
Rin Takanashi <dareka826@gmail.com>
2022-09-06 08:46:08 +0200

    nwg-wrapper: finally decided on which color to use for lockscreen clock

--- nwg-wrapper/.config/nwg-wrapper/waylock-time.sh
+++ nwg-wrapper/.config/nwg-wrapper/waylock-time.sh
@@ -8,8 +8,6 @@ date_raw="$(date +'%a, %d/%m/%Y|%H:%M:%S')"
 date="${date_raw%%|*}"
 time="${date_raw##*|}"
 
-printf '<span size="%s" foreground="%s" face="%s">%s</span>\n' "${LARGE_TEXT}" "#ff8952" "${FONT}" "${time}"
-printf '<span size="%s" foreground="%s" face="%s">%s</span>\n' "${LARGE_TEXT}" "#ff5c9a" "${FONT}" "${time}"
 printf '<span size="%s" foreground="%s" face="%s">%s</span>\n' "${LARGE_TEXT}" "#ff8ab7" "${FONT}" "${time}"
 printf '<span size="%s" foreground="%s" face="%s">%s</span>\n' "${SMALL_TEXT}" "#eeeeee" "${FONT}" "${date}"
 printf '<span size="%s" foreground="%s" face="%s">%s</span>\n' "${SMALL_TEXT}" "#eeeeee" "${FONT}" "$(uptime -p)"
