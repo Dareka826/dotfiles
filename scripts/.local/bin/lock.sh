005684bc8ced382c5c9373b7bdc6a246e67851c7
Rin Takanashi <dareka826@gmail.com>
2022-09-09 18:33:21 +0200

    scripts: lock.sh: tabs to spaces

--- scripts/.local/bin/lock.sh
+++ scripts/.local/bin/lock.sh
@@ -1,51 +1,51 @@
 #!/bin/sh
 
 i3lock \
-	-i ~/Wallpapers/lockscreen2.png \
-	--indicator \
-	--force-clock \
-	-S 0 \
+    -i ~/Wallpapers/lockscreen2.png \
+    --indicator \
+    --force-clock \
+    -S 0 \
 \
-	--ringver-color=222222ff \
-	--ringwrong-color=EE99BB \
-	--ring-color=DD5588FF \
+    --ringver-color=222222ff \
+    --ringwrong-color=EE99BB \
+    --ring-color=DD5588FF \
 \
-	--keyhl-color=FFFFFFFF \
-	--bshl-color=111111ff \
+    --keyhl-color=FFFFFFFF \
+    --bshl-color=111111ff \
 \
-	--insidever-color=00000000 \
-	--insidewrong-color=00000000 \
-	--inside-color=00000000 \
+    --insidever-color=00000000 \
+    --insidewrong-color=00000000 \
+    --inside-color=00000000 \
 \
-	--line-color=00000000 \
-	--separator-color=00000000 \
+    --line-color=00000000 \
+    --separator-color=00000000 \
 \
-	--verif-color=00000000 \
-	--wrong-color=00000000 \
+    --verif-color=00000000 \
+    --wrong-color=00000000 \
 \
-	--time-color=DD5588FF \
-	--date-color=FF22AAFF \
+    --time-color=DD5588FF \
+    --date-color=FF22AAFF \
 \
-	--date-str="%A %d/%m/%Y" \
+    --date-str="%A %d/%m/%Y" \
 \
-	--time-align 2 \
-	--date-align 2 \
+    --time-align 2 \
+    --date-align 2 \
 \
-	--time-font="Source Code Pro"\
-	--date-font="Source Code Pro" \
+    --time-font="Source Code Pro"\
+    --date-font="Source Code Pro" \
 \
-	--time-size=80 \
-	--date-size=15 \
+    --time-size=80 \
+    --date-size=15 \
 \
-	--radius 50 \
-	--ring-width 15 \
+    --radius 50 \
+    --ring-width 15 \
 \
-	--ind-pos="x+r+20:y+h-r-20" \
-	--time-pos="x+w-25:y+h-55" \
-	--date-pos="tx:ty+30" \
+    --ind-pos="x+r+20:y+h-r-20" \
+    --time-pos="x+w-25:y+h-55" \
+    --date-pos="tx:ty+30" \
 \
-	--verif-text="" \
-	--wrong-text="" \
-	--noinput-text="" \
-	--lock-text="" \
-	--lockfailed-text="" || i3lock || pkill -KILL Xorg
+    --verif-text="" \
+    --wrong-text="" \
+    --noinput-text="" \
+    --lock-text="" \
+    --lockfailed-text="" || i3lock || pkill -KILL Xorg
