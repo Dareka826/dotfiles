e969a73d68c010787e600a5b2755e499e0b5f591
Rin Takanashi <dareka826@gmail.com>
2022-10-24 23:35:14 +0200

    awesome: webcord window rules

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -518,6 +518,10 @@ awful.rules.rules = {
     -- Set Firefox to always map on the tag named "2" on screen 1.
     -- { rule = { class = "Firefox" },
     --   properties = { screen = 1, tag = "2" } },
+
+    -- Don't maximize WebCord
+    { rule = { class = "WebCord" },
+        properties = { maximized = false } }
 }
 -- }}}
 
