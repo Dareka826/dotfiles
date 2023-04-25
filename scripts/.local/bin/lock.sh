e2dc74ba0ac2853dad505e60616f4feab572477a
Rin Brk <dareka826@gmail.com>
2023-04-25 19:06:43 +0200

    scripts: update lock.sh

--- scripts/.local/bin/lock.sh
+++ scripts/.local/bin/lock.sh
@@ -1,45 +1,15 @@
 #!/bin/sh
 
-i3lock \
-    --color=000000ff \
-    --force-clock \
-\
-    --ring-color=ff8ab7ff \
-    --ringver-color=333333ff \
-    --ringwrong-color=ff6600ff \
-\
-    --keyhl-color=ffffffff \
-    --bshl-color=333333ff \
-\
-    --inside-color=00000000 \
-    --insidever-color=00000000 \
-    --insidewrong-color=00000000 \
-\
-    --line-color=00000000 \
-    --separator-color=00000000 \
-\
-    --time-color=ff8ab7ff \
-    --date-color=eeeeeeff \
-\
-    --date-str="%a, %d/%m/%Y" \
-\
-    --time-align 1 \
-    --date-align 1 \
-\
-    --time-font="Source Code Pro"\
-    --date-font="Source Code Pro" \
-\
-    --time-size=60 \
-    --date-size=20 \
-\
-    --radius 50 \
-    --ring-width 10 \
-\
-    --time-pos="x+25:y+h-55" \
-    --date-pos="tx:ty+30" \
-\
-    --verif-text="" \
-    --wrong-text="" \
-    --noinput-text="" \
-    --lock-text="" \
-    --lockfailed-text="" || i3lock || pkill -KILL Xorg
+#{ printf "%s\n" "lock" | nc -q0 DPC 32454 || :; } &
+
+exec env \
+    XSECURELOCK_AUTH_BACKGROUND_COLOR='#111111' \
+    XSECURELOCK_AUTH_FOREGROUND_COLOR='#FFFFFF' \
+    XSECURELOCK_AUTH_WARNING_COLOR='#FF6600' \
+    XSECURELOCK_BACKGROUND_COLOR='#000000' \
+    XSECURELOCK_BLANK_TIMEOUT='-1' \
+    XSECURELOCK_BLANK_DPMS_STATE='on' \
+    XSECURELOCK_DATETIME_FORMAT='%Y-%m-%d %H:%M:%S' \
+    XSECURELOCK_SHOW_DATETIME=1 \
+    XSECURELOCK_AUTH_CURSOR_BLINK=0 \
+        xsecurelock
