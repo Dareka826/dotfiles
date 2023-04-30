21d368999aae1927f804d1184863b1b6d723eee3
Rin Brk <dareka826@gmail.com>
2023-04-30 14:06:55 +0200

    scripts: update btrfs_scrub

--- scripts/btrfs_scrub.sh
+++ scripts/btrfs_scrub.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
+set -eu
 
-[ "$1" ] || exit 0
+BTRFS_PATH="${1}"
+[ -n "${BTRFS_PATH}" ] || exit 1
 
-mountpoint="$1"
+shift 1
+SNOOZE_ARGS="${@}"
+[ -n "${SNOOZE_ARGS}" ] || exit 1
 
-grep "$mountpoint" /proc/mounts 2>/dev/null 1>&2 || {
-    echo "[E]: Path not in /proc/mounts"
-    exit 1
-}
+TIMEFILE_ID="$(printf "%s %s" "${BTRFS_PATH}" "${SNOOZE_ARGS}" | md5sum | awk '{print $1}')"
+TIMEFILE_PATH="/var/snooze/btrfs_scrub_${TIMEFILE_ID}_timefile"
 
-# clean up completely unused block groups
-btrfs balance start -musage=0 -v "$mountpoint" 2>&1
-btrfs balance start -dusage=0 -v "$mountpoint" 2>&1
+SCRUB_STATUS="$(btrfs scrub status "${BTRFS_PATH}" | grep '^Status' | awk '{print $2}')"
+SCRUB_LOG=""
+SCRUB_EXIT=""
 
-# re-write data block groups with usage under 20% into new locations
-# and try to compact them together
-btrfs balance start -dusage=20 -v "$mountpoint" 2>&1
+printf "[I]: Checking for a running scrub on %s...\n" "${BTRFS_PATH}"
+if [ "${SCRUB_STATUS}" = "running" ]; then
+    # Abort and resume to know when it ends
+    printf "[I]: Found a running scrub on %s, attaching...\n" "${BTRFS_PATH}"
+    btrfs scrub cancel "${BTRFS_PATH}"
+    SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)"
+    SCRUB_EXIT="$?"
+else
+    printf "[I]: Checking for an aborted scrub on %s...\n" "${BTRFS_PATH}"
+    if [ "${SCRUB_STATUS}" = "aborted" ]; then
 
-# idle (3) io scheduling class
-# low (10) cpu scheduling class
-# verify all checksums
-time \
-    nice -n 10 \
-        btrfs scrub start -c 3 -B -d "$mountpoint"
+        printf "[I]: Found an aborted scrub on %s, resuming...\n" "${BTRFS_PATH}"
+        SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)"
+    SCRUB_EXIT="$?"
+    else
+        # Scrub not running or aborted, start a new one
+        printf "[I]: Waiting to start a new scrub on %s...\n" "${BTRFS_PATH}"
+        snooze -t "${TIMEFILE_PATH}" "${SNOOZE_ARGS}"
+
+        printf "[I]: Starting a new scrub on %s...\n" "${BTRFS_PATH}"
+        SCRUB_LOG="$(nice -n 10 btrfs scrub start -c 3 -B "${BTRFS_PATH}" 2>&1)"
+    SCRUB_EXIT="$?"
+    fi
+fi
+
+# Check for errors
+if [ "${SCRUB_EXIT}" != "0" ]; then
+    printf "${SCRUB_LOG}\n" | mailx -s "BTRFS Scrub Error" root rin
+fi
+
+# The timefile should only be updated after a scrub finishes
+[ -d "/var/snooze" ] || mkdir -p "/var/snooze"
+touch "${TIMEFILE_PATH}"
