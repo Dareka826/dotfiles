a1172cce44ada648ce0b6fc18b33493cf4337a5b
Rin Brk <dareka826@gmail.com>
2023-05-16 15:49:40 +0200

    nvim: fix weird blue indent

--- nvim/.config/nvim/after/plugin/indent.lua
+++ nvim/.config/nvim/after/plugin/indent.lua
@@ -1,9 +1,11 @@
 -- Indent lines
-vim.cmd('highlight IndentBlanklineContextChar guifg=#8877FF gui=nocombine')
-
 require("indent_blankline").setup({
     show_trailing_blankline_indent = false,
     show_current_context = true,
     show_first_indent_level = false,
     use_treesitter = true,
 })
+
+vim.cmd('highlight IndentBlanklineChar               cterm=nocombine gui=nocombine guifg=#3b4261')
+vim.cmd('highlight IndentBlanklineSpaceChar          cterm=nocombine gui=nocombine guifg=#3b4261')
+vim.cmd('highlight IndentBlanklineSpaceCharBlankline cterm=nocombine gui=nocombine guifg=#3b4261')
