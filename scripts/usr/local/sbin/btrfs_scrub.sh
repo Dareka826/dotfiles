771f6d8a32649b2295f97726c9b458409ae4afe3
Rin Brk <dareka826@gmail.com>
2023-05-16 02:12:06 +0200

    scripts: fix btrfs_scrub when non-zero exit and timefile updating

--- scripts/usr/local/sbin/btrfs_scrub.sh
+++ scripts/usr/local/sbin/btrfs_scrub.sh
@@ -20,23 +20,23 @@ if [ "${SCRUB_STATUS}" = "running" ]; then
     # Abort and resume to know when it ends
     printf "[I]: Found a running scrub on %s, attaching...\n" "${BTRFS_PATH}"
     btrfs scrub cancel "${BTRFS_PATH}"
-    SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)"
-    SCRUB_EXIT="$?"
+    SCRUB_EXIT="0"
+    SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
 else
     printf "[I]: Checking for an aborted scrub on %s...\n" "${BTRFS_PATH}"
     if [ "${SCRUB_STATUS}" = "aborted" ]; then
 
         printf "[I]: Found an aborted scrub on %s, resuming...\n" "${BTRFS_PATH}"
-        SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)"
-    SCRUB_EXIT="$?"
+        SCRUB_EXIT="0"
+        SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
     else
         # Scrub not running or aborted, start a new one
         printf "[I]: Waiting to start a new scrub on %s...\n" "${BTRFS_PATH}"
         snooze -t "${TIMEFILE_PATH}" "${SNOOZE_ARGS}"
 
         printf "[I]: Starting a new scrub on %s...\n" "${BTRFS_PATH}"
-        SCRUB_LOG="$(nice -n 10 btrfs scrub start -c 3 -B "${BTRFS_PATH}" 2>&1)"
-    SCRUB_EXIT="$?"
+        SCRUB_EXIT="0"
+        SCRUB_LOG="$(nice -n 10 btrfs scrub start -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
     fi
 fi
 printf "[I]: Scrub on %s finished with exit code: %s\n" "${BTRFS_PATH}" "${SCRUB_EXIT}"
@@ -46,7 +46,10 @@ if [ "${SCRUB_EXIT}" != "0" ]; then
     printf "${SCRUB_LOG}\n" | mailx -s "BTRFS Scrub Error" root rin
 fi
 
-# The timefile should only be updated after a scrub finishes
-[ -d "/var/snooze" ] || mkdir -p "/var/snooze"
-printf "[I]: Updating timefile...\n"
-touch "${TIMEFILE_PATH}"
+SCRUB_STATUS="$(btrfs scrub status "${BTRFS_PATH}" | grep '^Status' | awk '{print $2}')"
+# If scrub has finished (not aborted), update timefile
+if [ "${SCRUB_STATUS}" = "finished" ]; then
+    [ -d "/var/snooze" ] || mkdir -p "/var/snooze"
+    printf "[I]: Updating timefile...\n"
+    touch "${TIMEFILE_PATH}"
+fi
