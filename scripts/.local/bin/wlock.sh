b17f76204b33ae07350cb291f4536068669d6b93
Rin Brk <dareka826@gmail.com>
2023-06-13 01:05:38 +0200

    scripts: update wlock.sh

--- scripts/.local/bin/wlock.sh
+++ scripts/.local/bin/wlock.sh
@@ -1,22 +1,9 @@
 #!/bin/sh
 
-{ # Display time and date on lockscreen
-    sleep 0.1
-    nwg-wrapper \
-        --layer 3 \
-        --script waylock-time.sh \
-        --css waylock.css \
-        --refresh 1000 \
-        --margin_bottom 25 \
-        --margin_left   25 \
-        --justify left \
-        --position left \
-        --alignment end \
-        --sig_quit 31 # Exit on custom signal
-} &
-makoctl mode -a locked # Make notifications invisible
+# Make notifications invisible
+makoctl mode -a locked
 
-waylock --init-color '#000000' --input-color '#6633ff' --fail-color '#ff6600' || pkill river
+waylock -init-color '0x000000' -input-color '0x6633ff' -fail-color '0xff6600'
 
-pkill -f --signal 31 nwg-wrapper & # Kill time+date widget
-makoctl mode -r locked & # Make notifications visible again
+# Make notifications visible again
+makoctl mode -r locked
