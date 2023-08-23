bdb67524205f45fa87819f6cd4d860c82c2521bc
Rin Brk <dareka826@gmail.com>
2023-08-23 12:43:12 +0200

    nvim: change cmp sources priority

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -841,9 +841,7 @@ do
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       { name = 'luasnip' },
-    }, {
       { name = 'nvim_lsp_signature_help' },
-    }, {
       { name = 'buffer' },
       { name = 'path' },
     }),
