908f97d7d900f9e107be260f5c56acfa2f40eb8d
Rin Brk <dareka826@gmail.com>
2024-05-23 05:23:04 +0200

    scripts: rework btrfs_scrub.sh

--- scripts/usr/local/sbin/btrfs_scrub.sh
+++ scripts/usr/local/sbin/btrfs_scrub.sh
@@ -1,55 +1,43 @@
 #!/bin/sh
 set -eu
 
-BTRFS_PATH="${1}"
-[ -n "${BTRFS_PATH}" ] || exit 1
-
+MOUNT_POINT="${1}"
+[ -n "${MOUNT_POINT}" ] || exit 1
+[ -d "${MOUNT_POINT}" ] || exit 1
 shift 1
-SNOOZE_ARGS="${@}"
+
+SNOOZE_ARGS="$(printf "%s " "$@" | sed 's/ \+$//')"
 [ -n "${SNOOZE_ARGS}" ] || exit 1
 
-TIMEFILE_ID="$(printf "%s %s" "${BTRFS_PATH}" "${SNOOZE_ARGS}" | md5sum | awk '{print $1}')"
+TIMEFILE_ID="$(printf "%s %s" "${MOUNT_POINT}" "${SNOOZE_ARGS}" | md5sum | awk '{print $1}')"
 TIMEFILE_PATH="/var/snooze/btrfs_scrub_${TIMEFILE_ID}_timefile"
 
-SCRUB_STATUS="$(btrfs scrub status "${BTRFS_PATH}" | grep '^Status' | awk '{print $2}')"
-SCRUB_LOG=""
-SCRUB_EXIT=""
+printf "[I]: Trying to cancel scrub on %s...\n" "${MOUNT_POINT}"
+btrfs scrub cancel "${MOUNT_POINT}" >/dev/null 2>&1 || :
 
-printf "[I]: Checking for a running scrub on %s...\n" "${BTRFS_PATH}"
-if [ "${SCRUB_STATUS}" = "running" ]; then
-    # Abort and resume to know when it ends
-    printf "[I]: Found a running scrub on %s, attaching...\n" "${BTRFS_PATH}"
-    btrfs scrub cancel "${BTRFS_PATH}"
+printf "[I]: Trying to resume scrub on %s...\n" "${MOUNT_POINT}"
+SCRUB_EXIT="0"
+SCRUB_LOG="$(set +e; nice -n 10 btrfs scrub resume -c 3 -d -B "${MOUNT_POINT}" 2>&1)" || SCRUB_EXIT="$?"
+
+if [ "${SCRUB_EXIT}" = "2" ]; then
+    # There was nothing to resume
+    printf "[I]: Waiting to start a new scrub on %s...\n" "${MOUNT_POINT}"
+    snooze -t "${TIMEFILE_PATH}" "${SNOOZE_ARGS}"
+
+    printf "[I]: Starting a new scrub on %s...\n" "${MOUNT_POINT}"
     SCRUB_EXIT="0"
-    SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
-else
-    printf "[I]: Checking for an aborted scrub on %s...\n" "${BTRFS_PATH}"
-    if [ "${SCRUB_STATUS}" = "aborted" ]; then
-
-        printf "[I]: Found an aborted scrub on %s, resuming...\n" "${BTRFS_PATH}"
-        SCRUB_EXIT="0"
-        SCRUB_LOG="$(nice -n 10 btrfs scrub resume -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
-    else
-        # Scrub not running or aborted, start a new one
-        printf "[I]: Waiting to start a new scrub on %s...\n" "${BTRFS_PATH}"
-        snooze -t "${TIMEFILE_PATH}" "${SNOOZE_ARGS}"
-
-        printf "[I]: Starting a new scrub on %s...\n" "${BTRFS_PATH}"
-        SCRUB_EXIT="0"
-        SCRUB_LOG="$(nice -n 10 btrfs scrub start -c 3 -B "${BTRFS_PATH}" 2>&1)" || SCRUB_EXIT="$?"
-    fi
+    SCRUB_LOG="$(set +e; nice -n 10 btrfs scrub start -c 3 -B "${MOUNT_POINT}" 2>&1)" || SCRUB_EXIT="$?"
 fi
-printf "[I]: Scrub on %s finished with exit code: %s\n" "${BTRFS_PATH}" "${SCRUB_EXIT}"
+
+printf "[I]: Scrub on %s finished with exit code: %s\n" "${MOUNT_POINT}" "${SCRUB_EXIT}"
 
 # Check for errors
 if [ "${SCRUB_EXIT}" != "0" ]; then
     printf "${SCRUB_LOG}\n" | mailx -s "BTRFS Scrub Error" root rin
 fi
 
-SCRUB_STATUS="$(btrfs scrub status "${BTRFS_PATH}" | grep '^Status' | awk '{print $2}')"
-# If scrub has finished (not aborted), update timefile
-if [ "${SCRUB_STATUS}" = "finished" ]; then
-    [ -d "/var/snooze" ] || mkdir -p "/var/snooze"
-    printf "[I]: Updating timefile...\n"
-    touch "${TIMEFILE_PATH}"
-fi
+printf "[I]: Updating timefile...\n"
+# Might be touched if scrub was aborted, but we try to resume every time,
+# so it shouldn't matter
+[ -d "/var/snooze" ] || mkdir -p "/var/snooze"
+touch "${TIMEFILE_PATH}"
