db7f7c7924b1a32d4bbe7da4749829d083d357f0
Rin Brk <dareka826@gmail.com>
2025-03-18 22:49:06 +0100

    themes: update oxocarbon

--- color-themes/themes/tty/oxocarbon.theme.sh
+++ color-themes/themes/tty/oxocarbon.theme.sh
@@ -2,20 +2,20 @@
 
 [ $TERM = "linux" ] || exit 0
 
-printf '\033]P0262626
+printf '\033]P0161616
         \033]P8525252
         \033]P1ee5396
         \033]P9ee5396
         \033]P242be65
         \033]PA42be65
-        \033]P378a9ff
-        \033]PB78a9ff
+        \033]P3ff7eb6
+        \033]PBff7eb6
         \033]P433b1ff
         \033]PC33b1ff
         \033]P5be95ff
         \033]PDbe95ff
         \033]P63ddbd9
         \033]PE3ddbd9
-        \033]P7f2f2f2
+        \033]P7f2f4f8
         \033]PFffffff
         \033c'
