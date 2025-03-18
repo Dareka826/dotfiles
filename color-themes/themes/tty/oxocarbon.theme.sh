e597d3467c9c26c86c0ab2a706bca1a7aba7bf83
Rin Brk <dareka826@gmail.com>
2025-03-18 15:21:07 +0100

    themes: add oxocarbon

---
+++ color-themes/themes/tty/oxocarbon.theme.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+[ $TERM = "linux" ] || exit 0
+
+printf '\033]P0262626
+        \033]P8525252
+        \033]P1ee5396
+        \033]P9ee5396
+        \033]P242be65
+        \033]PA42be65
+        \033]P378a9ff
+        \033]PB78a9ff
+        \033]P433b1ff
+        \033]PC33b1ff
+        \033]P5be95ff
+        \033]PDbe95ff
+        \033]P63ddbd9
+        \033]PE3ddbd9
+        \033]P7f2f2f2
+        \033]PFffffff
+        \033c'
