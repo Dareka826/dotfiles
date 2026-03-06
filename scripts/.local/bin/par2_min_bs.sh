78c040dba7dae846a17aa1aa13322a3744a0bd0a
Rin Brk <dareka826@gmail.com>
2026-03-06 12:55:03 +0100

    scripts: update par2_min_bs.sh

--- scripts/.local/bin/par2_min_bs.sh
+++ scripts/.local/bin/par2_min_bs.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 set -eu
 
-[ "$#" = 2 ] || {
-    printf "%s\n" \
-        "Usage:" \
-        "  ./par2_min_bs.sh  FILE  RECV_BLK_COUNT" \
-        ""
+p()  { printf '%s\n' "$@"; }
+pn() { printf '%s' "$@"; }
+
+PROG_NAME="${0}"
+PROG_NAME="${PROG_NAME##*/}"
+
+if ! [ "${#}" = 2 ]; then
+    p "Usage:" \
+      "  ${PROG_NAME}  FILE  RECV_BLK_COUNT" \
+      ""
+
     exit 0
-}
+fi
 
 FILE="${1}"
-N_BLK="${2}"
+BLK_CNT="${2}"
 
-[ -e "${FILE}" ] || {
-    printf "%s\n" "[E]: No such file: ${FILE}"
+if ! [ "$(pn "x${BLK_CNT}x" | tr -d "0-9")" = "xx" ]; then
+    p "[E]: Not an integer: ${BLK_CNT}"
     exit 1
-}
+fi
 
-[ "$(printf "%s" "${N_BLK}" | tr -d '0-9')" = "" ] || {
-    printf "%s\n" "[E]: Not an integer: ${N_BLK}"
+if ! [ "$(pn "x${BLK_CNT}" | head -c2 | tr "1-9" "x")" = "xx" ]; then
+    p "[E]: Not a valid integer: ${BLK_CNT}"
     exit 1
-}
+fi
 
-FILE_SIZE="$(du -b "${FILE}" | grep -Eo '^[0-9]+')"
-[ "${FILE_SIZE}" != 0 ] || {
-    printf "%s\n" "[W]: Empty file: ${FILE}"
+if ! [ -f "${FILE}" ]; then
+    p "[E]: No such file: ${FILE}"
+    exit 1
+fi
+
+FILE_SIZE="$(du -b "${FILE}" | sed 's/\s.*$//')"
+if [ "${FILE_SIZE}" = 0 ]; then
+    p "[E]: Empty file: ${FILE}"
     exit 2
-}
+fi
 
 # ceil(sqrt((rec_blk_cnt+1)/rec_blk_cnt * 20 * file_size))
-MIN_BLK_SIZE="$(awk 'BEGIN { r = '"${N_BLK}"'; x = sqrt(((r+1) / r) * 20 * '"${FILE_SIZE}"'); if (int(x) != x) { x += 1 }; print(int(x)) }')"
+MIN_BLK_SIZE="$(awk 'BEGIN { r = '"${BLK_CNT}"'; x = sqrt(((r+1) / r) * 20 * '"${FILE_SIZE}"'); if (int(x) != x) { x += 1 }; print(int(x)) }')"
 # Round up to a mult of 4
 MIN_BLK_SIZE="$(awk 'BEGIN { print( int(('"${MIN_BLK_SIZE}"' + 3) / 4) * 4 ) }')"
 
-printf "%s\n" "${MIN_BLK_SIZE}"
+p "${MIN_BLK_SIZE}"
