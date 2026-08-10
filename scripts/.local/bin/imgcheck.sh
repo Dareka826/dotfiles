caa0f77b472239bbde97be49dd9542524acd640d
Rin Brk <dareka826@gmail.com>
2026-08-10 12:35:49 +0200

    scripts: fix imgcheck.sh

--- scripts/.local/bin/imgcheck.sh
+++ scripts/.local/bin/imgcheck.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
 set -eu
 
-IM_PROG="magick"
-command -v magick >/dev/null 2>&1 || \
-    IM_PROG="convert"
-command -v convert >/dev/null 2>&1 || \
+IM_PROG=""
+
+if command -v magick >/dev/null 2>&1; then
+    IM_PROG="magick identify"
+elif command -v identify >/dev/null 2>&1; then
+    IM_PROG="dentify"
+else
+    printf '%s\n' "[E]: Neither \`magick' nor \`identify' programs found!" >&2
     exit 100
+fi
 
 for IMG_FILE in "$@"; do
-    [ -e "${IMG_FILE}" ] || continue
+    if ! [ -e "${IMG_FILE}" ]; then
+        printf '%s\n' "[W]: Skipping \`${IMG_FILE}' as it does not exist" >&2
+        continue
+    fi
 
-    if "${IM_PROG}" "${IMG_FILE}" null:-; then
-        printf "OK: %s\n" "${IMG_FILE}"
+    if ${IM_PROG} -regard-warnings -format '' "${IMG_FILE}" 2>&1; then
+        printf "[OK ]: %s\n" "${IMG_FILE}"
     else
-        printf "ERR: %s\n" "${IMG_FILE}"
+        printf "[ERR]: %s\n" "${IMG_FILE}"
     fi
 done
