bb2ba3d4beb9dfbcfdf2cad7a27c36ae946e82b4
Rin Brk <dareka826@gmail.com>
2026-09-09 16:40:17 +0200

    nvim: lua/fnl-compile.lua: wrap everything in a function to easily re-compile and optionally specify custom fennel.lua path

--- nvim/.config/nvim/lua/fnl-compile.lua
+++ nvim/.config/nvim/lua/fnl-compile.lua
@@ -1,115 +1,123 @@
+local M = {}
+
 local confdir = vim.fn.stdpath('config')
 local fnldir = confdir .. '/fnl'
 local compiledir = confdir .. '/lua/fnl'
 
-local fennelpath = os.getenv("HOME") .. '/.local/pkg/fennel/current/fennel.lua'
+M.fennelpath = os.getenv("HOME") .. '/.local/pkg/fennel/current/fennel.lua'
 
-if not vim.uv.fs_stat(fnldir) then
-  return nil
-end
+function M.compile(custom_fennelpath)
+  local fennelpath = (custom_fennelpath or M.fennelpath)
 
-local files_string = vim.fn.system({
-  'find', fnldir, '-type', 'f', '-iname', '*.fnl'
-})
-if vim.v.shell_error ~= 0 then
-  vim.api.nvim_echo({
-    { "`find' failed while searching for `*.fnl' in `" .. fnldir .. "'.", "ErrorMsg" },
-    { "\nPress any key to exit..." },
-  }, true, {})
-  vim.fn.getchar()
-  os.exit(1)
-end
-
-local files = {}
-for path in files_string:gmatch('(.-)\n') do
-  table.insert(files, path)
-end
-
-local function escape_pattern(str)
-  return (str:gsub('[%^%$%(%)%%%.%[%]%*%+%-%?]', '%%%1'))
-end
-local function escape_repl(str)
-  return (str:gsub('%%', '%%%%'))
-end
-
-local fennel = nil
-
-for _, fnl_path in ipairs(files) do
-  local lua_path = fnl_path:gsub('^' .. escape_pattern(fnldir) .. '(.+)%.fnl$', escape_repl(compiledir) .. '%1.lua')
-  local lua_stat = vim.uv.fs_stat(lua_path)
-
-  local do_compile = false
-
-  if not lua_stat then
-    do_compile = true
-  else
-    local fnl_stat = vim.uv.fs_stat(fnl_path)
-
-    if not fnl_stat then
-      vim.api.nvim_echo({
-        { "Failed to stat `" .. fnl_path .. "'.", "ErrorMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
-    end
-
-    if ((fnl_stat.mtime.sec > lua_stat.mtime.sec) or (fnl_stat.mtime.sec == lua_stat.mtime.sec and fnl_stat.mtime.nsec > lua_stat.mtime.nsec)) then
-      do_compile = true
-    end
+  if not vim.uv.fs_stat(fnldir) then
+    return nil
   end
 
-  if do_compile then
-    if not vim.uv.fs_stat(compiledir) then
-      if not vim.uv.fs_mkdir(compiledir, tonumber('755', 8)) then
+  local files_string = vim.fn.system({
+    'find', fnldir, '-type', 'f', '-iname', '*.fnl'
+  })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "`find' failed while searching for `*.fnl' in `" .. fnldir .. "'.", "ErrorMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+
+  local files = {}
+  for path in files_string:gmatch('(.-)\n') do
+    table.insert(files, path)
+  end
+
+  local function escape_pattern(str)
+    return (str:gsub('[%^%$%(%)%%%.%[%]%*%+%-%?]', '%%%1'))
+  end
+  local function escape_repl(str)
+    return (str:gsub('%%', '%%%%'))
+  end
+
+  local fennel = nil
+
+  for _, fnl_path in ipairs(files) do
+    local lua_path = fnl_path:gsub('^' .. escape_pattern(fnldir) .. '(.+)%.fnl$', escape_repl(compiledir) .. '%1.lua')
+    local lua_stat = vim.uv.fs_stat(lua_path)
+
+    local do_compile = false
+
+    if not lua_stat then
+      do_compile = true
+    else
+      local fnl_stat = vim.uv.fs_stat(fnl_path)
+
+      if not fnl_stat then
         vim.api.nvim_echo({
-          { "Failed to mkdir `" .. compiledir .. "'.", "ErrorMsg" },
+          { "Failed to stat `" .. fnl_path .. "'.", "ErrorMsg" },
           { "\nPress any key to exit..." },
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
       end
+
+      if ((fnl_stat.mtime.sec > lua_stat.mtime.sec) or (fnl_stat.mtime.sec == lua_stat.mtime.sec and fnl_stat.mtime.nsec > lua_stat.mtime.nsec)) then
+        do_compile = true
+      end
     end
 
-    local fnl_fh = io.open(fnl_path, 'r')
-    if not fnl_fh then
-      vim.api.nvim_echo({
-        { "Failed to open `" .. fnl_path .. "'.", "ErrorMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
+    if do_compile then
+      if not vim.uv.fs_stat(compiledir) then
+        if not vim.uv.fs_mkdir(compiledir, tonumber('755', 8)) then
+          vim.api.nvim_echo({
+            { "Failed to mkdir `" .. compiledir .. "'.", "ErrorMsg" },
+            { "\nPress any key to exit..." },
+          }, true, {})
+          vim.fn.getchar()
+          os.exit(1)
+        end
+      end
+
+      local fnl_fh = io.open(fnl_path, 'r')
+      if not fnl_fh then
+        vim.api.nvim_echo({
+          { "Failed to open `" .. fnl_path .. "'.", "ErrorMsg" },
+          { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+      end
+
+      if not fennel then
+        package.path = package.path .. ';' .. fennelpath
+        fennel = require('fennel')
+      end
+
+      vim.print("Compiling `" .. fnl_path:gsub('^' .. escape_pattern(confdir .. '/'), '') .. "'...")
+
+      local lua_out = fennel.compile(fnl_fh, {})
+      fnl_fh:close()
+      if not lua_out then
+        vim.api.nvim_echo({
+          { "Failed to compile `" .. fnl_path .. "'.", "ErrorMsg" },
+          { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+      end
+
+      local lua_fh = io.open(lua_path, 'w')
+      if not lua_fh then
+        vim.api.nvim_echo({
+          { "Failed to open `" .. lua_path .. "'.", "ErrorMsg" },
+          { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+      end
+
+      lua_fh:write(lua_out)
+      lua_fh:close()
     end
-
-    if not fennel then
-      package.path = package.path .. ';' .. fennelpath
-      fennel = require('fennel')
-    end
-
-    vim.print("Compiling `" .. fnl_path:gsub('^' .. escape_pattern(confdir .. '/'), '') .. "'...")
-
-    local lua_out = fennel.compile(fnl_fh, {})
-    fnl_fh:close()
-    if not lua_out then
-      vim.api.nvim_echo({
-        { "Failed to compile `" .. fnl_path .. "'.", "ErrorMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
-    end
-
-    local lua_fh = io.open(lua_path, 'w')
-    if not lua_fh then
-      vim.api.nvim_echo({
-        { "Failed to open `" .. lua_path .. "'.", "ErrorMsg" },
-        { "\nPress any key to exit..." },
-      }, true, {})
-      vim.fn.getchar()
-      os.exit(1)
-    end
-
-    lua_fh:write(lua_out)
-    lua_fh:close()
   end
 end
+
+return M
